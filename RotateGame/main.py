03127d747e3412a083435722b62732237eb70718
natemcoleman <112529346+natemcoleman@users.noreply.github.com>
2024-02-22 17:52:38 -0700

    moved previous files to same folder

--- RotateGame/main.py
+++ RotateGame/main.py
@@ -3,26 +3,22 @@ import pygame
 
 pygame.init()
 
-theCube = RotateLibrary.TheCube(32)
+theCube = RotateLibrary.TheCube(22)
+numRandomRotationsForSolution = 5
+width, height = 800, 600
+numRandomRotationsShuffle = 3
+
 solutionCube = RotateLibrary.TheCube(theCube.typeNum)
 
-width, height = 800, 600
 solutionScreen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption("2D Rubiks Cube")
 
-numRandomRotations = 5
-randomOrder = RotateLibrary.CreateRandomOrder(solutionCube, solutionScreen, numRandomRotations)
+randomOrder = RotateLibrary.CreateRandomOrder(solutionCube, solutionScreen, numRandomRotationsForSolution)
 RotateLibrary.ShowSolution(solutionCube, solutionScreen, randomOrder)
 
 moves = []
-width, height = 800, 600
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
-# Enable OpenGL acceleration for smoother rendering
-# pygame.display.gl_set_attribute(pygame.GL_MULTISAMPLEBUFFERS, 1)
-# pygame.display.gl_set_attribute(pygame.GL_MULTISAMPLESAMPLES, 4)
-
-# Game loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -35,16 +31,15 @@ while running:
             rotateCircleIndex = RotateLibrary.GetClosestLargeCircle(theCube, mouse_x, mouse_y)
             moves.append((rotateCircleIndex, keys[pygame.K_SPACE]))
             RotateLibrary.Rotate(screen, theCube, rotateCircleIndex, keys[pygame.K_SPACE], randomOrder, 0.75) #problem here
-            # RotateLibrary.CheckIfSolved(theCube, randomOrder)
+            RotateLibrary.CheckIfSolved(theCube, randomOrder)
 
         elif event.type == pygame.KEYDOWN:
             if pygame.key.get_pressed()[pygame.K_r]:
                 moves = []
-                randomOrder = RotateLibrary.CreateRandomOrder(solutionCube, solutionScreen, numRandomRotations)
+                randomOrder = RotateLibrary.CreateRandomOrder(solutionCube, solutionScreen, numRandomRotationsForSolution)
 
             elif pygame.key.get_pressed()[pygame.K_x]:
-                numRandomRotations2 = 3
-                moves = RotateLibrary.RandomizeCube(screen, theCube, numRandomRotations2, moves, randomOrder)
+                moves = RotateLibrary.RandomizeCube(screen, theCube, numRandomRotationsShuffle, moves, randomOrder)
 
             elif pygame.key.get_pressed()[pygame.K_a]:
                 moves = RotateLibrary.SolveCube(screen, theCube, moves, randomOrder)
